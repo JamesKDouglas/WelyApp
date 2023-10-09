6fe82118158c05a202f7c6058156d573beb90121
James Douglas <james@phytochemconsulting.ca>
2023-10-09 12:43:00 -0700

    work on Invest

--- controllers/home.js
+++ controllers/home.js
@@ -1,4 +1,10 @@
 module.exports = {
+  getInvest: (req, res) => {
+    res.render("invest.ejs");
+  },
+  getSplash: (req, res) => {
+    res.render("splash.ejs");
+  },
   getIndex: (req, res) => {
     res.render("index.ejs");
   },
--- routes/main.js
+++ routes/main.js
@@ -6,6 +6,8 @@ const postsController = require("../controllers/posts");
 const { ensureAuth, ensureGuest } = require("../middleware/auth");
 
 //Main Routes - simplified for now
+router.get("/", homeController.getSplash);
+router.get("/invest", homeController.getInvest);
 router.get("/app", homeController.getIndex);
 router.get("/profile", ensureAuth, postsController.getProfile);
 router.get("/feed", ensureAuth, postsController.getFeed);
